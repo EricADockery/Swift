66a43e3f01e939ec7a0c326a5682e85521e3ef37
EricADockery <eadock01@louisville.edu>
2016-01-21 23:46:15 -0500

    Starting Treehouse Swift
    Basic Swift language course

---
+++ TeamTreehouse Swift 2.0 Basics/SwiftBasics.playground/Contents.swift
@@ -0,0 +1,100 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+//var - variable keyword - mutable
+var str = "Hello, playground"
+str = "Hello, World!"
+
+var number = 10
+number = 20
+
+//let - constants - immuatable
+let language = "Swift"
+
+//Naming Conventions - cammelcasing
+let programmingLanguage = "Objective-C"
+let favLanguage = "Python"
+
+
+//Strings
+
+let country = "United States of America"
+let state = "North Carolina"
+let city = "Charlotte"
+let street = "West Street"
+
+//String concated
+let address = country+", "+state+", "+city
+
+//String interpolation
+let interpolatedAddress = "\(country), \(state), \(city)"
+let streetAddress = "222 \(street)"
+
+
+//Code challenge
+let name = "Eric"
+let greeting = "Hi there, \(name)."
+let finalGreeting = greeting + " How are you?"
+
+/*
+Integers
+*/
+
+var favoriteProgrammingLangage = "Swift"
+let year = 2014
+//Floats
+var version = 2.0 //Double
+//Boolean
+
+let isAwesome = true
+
+//Code Challenge
+// Enter your code below
+let title = "A Dance with Dragons"
+var rating = 7.5
+var isAvailable = false
+
+
+//Type Saftey - this seems for beginning programmers....
+var someString = " "
+
+let bestPlayer : String = "Micheal Jordan"
+let averagePointsPerGame : Double = 30.1
+
+// Code Challenge
+let firstValue = 1
+let secondValue = 1
+let product = firstValue * secondValue
+let output = "the product of \(firstValue) times \(secondValue) is \(product)"
+//Quiz
+
+
+// Arithmetic Operators - +-*% 
+let height = 12.0
+let width = 10.0
+let area = height * width
+
+let areaSqMeters = area/10.764
+
+//comparision == != > >= < <=
+
+//Challenge
+
+// Enter your code below
+let value = 200
+let divisor = 5
+
+let someOperation = 20 + 400 % 10 / 2 - 15
+let anotherOperation = 52 * 27 % 200 / 2 + 5
+
+// Task 1 - Enter your code below
+let result = value % divisor
+// Task 2 - Enter your code below
+let isPerfectMultiple = result == 0
+//Task3
+let isGreater = someOperation >= anotherOperation
+//increment and decrement ++ -- on the left or just use +=
+
+
+
+
