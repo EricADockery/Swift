c58631c4e7beedb50da5e3ab2881839c45143eb3
EricADockery <eadock01@louisville.edu>
2016-01-26 10:21:37 -0500

    Fixing Chapter 4
    Due to human error I created a sub project on Chapter 4 on Swift
    programming. I am fixing this issue.

---
+++ Big Nerd Ranch/Chapter 4/WorldTrotter_CH4/WorldTrotter/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Eric Dockery on 1/25/16.
+//  Copyright © 2016 Eric Dockery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+/* Practice code teaching how to programmatically create views on a UIView
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        
+        let firstFrame = CGRect(x:160, y: 240, width: 100, height: 150)
+        let firstView = UIView(frame: firstFrame)
+        firstView.backgroundColor = UIColor.blueColor()
+        view.addSubview(firstView)
+    
+        let secondFrame = CGRect(x: 20, y: 30, width: 50, height: 50)
+        let secondView = UIView(frame: secondFrame)
+        secondView.backgroundColor = UIColor.greenColor()
+        
+        //Added the view to the main UIView
+        //view.addSubview(secondView)
+        
+        //Add the view to the firstView
+        firstView.addSubview(secondView)
+        
+    }
+
+*/
+
+}
+
